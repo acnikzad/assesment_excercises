684afa5333b1c3d0cbf1c5e17bd0e005e416ef55
“Aria <a.c.nikzad@gmail.com>
2019-11-23 18:44:04 -0800

    finished arrays select practice

--- arrays_select_practice.rb
+++ arrays_select_practice.rb
@@ -73,7 +73,28 @@ new_array = []
 x = 0
 
 while x < array.length
-  if array[x].length < 4
+  word = array[x]
+
+  if word.length < 4
+    new_array << word
+  end
+  x += 1
+end
+
+p new_array 
+
+
+#  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
+new_array = []
+x = 0
+
+while x < array.length
+  word = array[x][:name]
+
+  if word.length < 6
     new_array << array[x]
   end
   x += 1
@@ -82,22 +103,73 @@ end
 p new_array 
 
 
-
-
-#  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
-#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
-
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+
+
+array = [3, 2, 3, 6, 27, 2, 4, 1, 8, 9, 4, 3, 6, 56, 32, 43, 87, 34]
+new_array = []
+x = 0
+
+while x < array.length
+  if array[x] < 10
+    new_array << array[x]
+  end
+  x +=1
+end
+
+p new_array
+
 
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
+array = ["Bear", "Apple", "Biscuit", "Lettuce", "Bam"]
+new_array = []
+x = 0
+
+while x < array.length
+  if array[x][0] == "B"
+    new_array << array[x]
+  end
+  x += 1
+end
+
+p new_array
+
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
+array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+new_array = []
+x =  0
+
+while x < array.length
+  if array[x][:price] < 10
+    new_array << array[x]
+  end
+  x += 1
+end
+
+p new_array
+
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+array = [5, 4, 3, 4, 7, 8, 9, 12, 14, 12, 13, 11, 9, 7]
+new_array = []
+x = 0
+
+while x < array.length
+  if array[x] % 2 == 1
+    new_array << array[x]
+  end
+  x +=1
+end
+
+p new_array
+
+
 
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/7de16ed43ea506e98df3fa15074b84f8
