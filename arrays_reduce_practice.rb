4d69f91f2b4389dcad7d920663ed934b0e190aaf
“Aria <a.c.nikzad@gmail.com>
2020-03-09 11:03:10 -0700

    problem7

---
+++ arrays_reduce_practice.rb
@@ -0,0 +1,120 @@
+#  1. Start with an array of numbers and compute the sum of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 26.
+
+array = [6, 76, 43, 23, 65, 78, 65, 12, 2, 1, 9, 89, 76]
+x = 0
+number = 0
+
+while x < array.length
+  number += array[x]
+  x += 1
+end
+
+p number
+
+#  2. Start with an array of strings and combine them all into a single string.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+array = ["BingBang", "BorkerPorker", "PoolTool"]
+word = ""
+x = 0
+
+while x < array.length
+  word = word + array[x]
+  x += 1
+end
+
+p word
+
+#  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
+array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+word = 0
+x = 0
+
+while x < array.length
+  word += array[x][:price]
+  x += 1
+end
+
+p word
+
+#  4. Start with an array of numbers and compute the the minumum number.
+#     For example, [5, 10, 8, 3] becomes 5.
+
+array = [6, 7, 2, 9, 4, 3, 9, 43]
+x = 0
+min = array[0]
+
+while x < array.length
+  if array[x] < min
+    min = array[x]
+  end
+  x += 1
+end
+
+p min
+
+
+#  5. Start with an array of strings and compute the total length of all the strings.
+#     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+array = ["Woody", "Buzz", "Andy", "Slinky", "Bo Peep", "Sid"]
+word = 0
+x = 0
+
+while x < array.length
+  word += array[x].length
+  x += 1
+end
+
+p word
+
+#  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+
+array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+min = array[0]
+x = 0
+
+while x < array.length
+  if array[x][:price] < min[:price]
+    min = array[x]
+  end
+  x += 1
+end
+
+p min 
+
+#  7. Start with an array of numbers and compute product of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 1200.
+
+array = [5, 10, 8, 3]
+x = 0
+y = 1
+
+while x < array.length
+  y = array[x] * y
+  x += 1
+end
+
+p y
+
+
+#  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+
+
+
+
+#  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+
+# 10. Start with an array of numbers and compute the maximum number.
+#     For example, [5, 10, 8, 3] becomes 10.
+
+
+# SOLUTIONS (using while loop): https://gist.github.com/peterxjang/376c8931a48549889eb3c9bc091e9f43
+# SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/379c9945774f51027750c59d6e4395df
+# SOLUTIONS (using .reduce shortcut): https://gist.github.com/peterxjang/b69183e2d555964ce3936893f423ef35
---
+++ converting_data_practice.rb
@@ -0,0 +1,164 @@
+#  1. Convert an array of arrays into a hash.
+#     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
+
+array = [[1, 3], [8, 9], [2, 16]]
+hash = {}
+x = 0
+
+while x < array.length
+  key = array[x][0]
+  value = array[x][1]
+  hash[key] = value
+  x += 1
+end
+
+p hash
+
+#  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
+#     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+
+array = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+hash = {}
+x = 0
+
+while x < array.length
+  hash[array[x][:id]] = array[x]
+  x += 1
+end
+
+p hash
+
+
+#  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
+#     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+
+string = "bookkeeper"
+hash = {}
+x = 0
+
+while x < string.length
+  letter = string[x]
+  if hash[letter] == nil
+    hash[letter] = 0
+  end
+  hash[letter] += 1
+  x += 1
+end
+
+p hash
+
+
+#  4. Convert a hash into an array of arrays.
+#     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+
+hash = {"chair" => 100, "book" => 14} 
+array =[]
+
+hash.each do |name, price|
+  array << [name, price]
+end
+
+p array
+
+#  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
+#     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
+
+hash = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
+array = []
+x = 0
+
+hash.each do |id, person|
+  person[:id] = id
+  array << person
+end
+
+p array
+
+
+#  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
+#     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
+
+array = ["do", "or", "do", "not"]
+hash = {}
+x = 0
+
+while x < array.length
+  word = array[x]
+  if hash[word] == nil
+    hash[word] = 0
+  end
+
+  hash[word] += 1
+  x += 1
+end
+
+p hash
+
+#  7. Convert a hash into a flat array containing all the hash’s keys and values.
+#     For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
+
+hash = {"a" => 1, "b" => 2, "c" => 3, "d" => 4}
+array = []
+x = 0
+
+hash.each do |key, value|
+  array << key
+  array << value
+end
+
+p array
+
+
+#  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
+#     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+
+prices = {"chair" => 75, "book" => 15}
+items = [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}]
+new_hash = {}
+x = 0
+
+while x < items.length
+  item = items[x]
+  name = item[:name]
+  color = item[:color]
+  weight = item[:weight]
+  price = prices[name]
+  new_hash[name] = {price: price, color: color, weight: weight}
+  x += 1
+end
+
+p new_hash
+
+#  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
+#     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
+
+books = [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}]
+books_hash = {}
+x = 0
+
+# while x < books.length
+#   book = books[x]
+#   author = books[:author]
+#   title = books[:title]
+#   if books_hash[author] == nil
+#     books_hash[author] = []
+#   end
+#   books_hash[author] << title
+#   index += 1
+# end
+
+# p books_hash
+
+# 10. Given a hash, create a new hash that has the keys and values switched.
+#     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}
+
+
+hash = {"a" => 1, "b" => 2, "c" => 3}
+new_hash = {}
+x = 0
+
+hash.each do |key, value|
+  new_hash[value] = key
+end
+
+p new_hash  
